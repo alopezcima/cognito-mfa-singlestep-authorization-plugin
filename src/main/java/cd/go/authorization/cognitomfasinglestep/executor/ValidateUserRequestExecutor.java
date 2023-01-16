ec18b9b5c4e031101bfab6b20f908bb1edeb0aff
Angel López Cima <angel.lopez.cima@bbva.com>
2023-01-16 17:39:13 +0100

    Update gradle up to 7.2

---
+++ src/main/java/cd/go/authorization/cognitomfasinglestep/executor/ValidateUserRequestExecutor.java
@@ -0,0 +1,16 @@
+package cd.go.authorization.cognitomfasinglestep.executor;
+
+import cd.go.authorization.cognitomfasinglestep.Authenticator;
+import com.thoughtworks.go.plugin.api.request.GoPluginApiRequest;
+import com.thoughtworks.go.plugin.api.response.GoPluginApiResponse;
+
+public class ValidateUserRequestExecutor implements RequestExecutor {
+    @Override
+    public GoPluginApiResponse execute() throws Exception {
+        return null;
+    }
+
+    public ValidateUserRequestExecutor(GoPluginApiRequest request) {
+
+    }
+}
